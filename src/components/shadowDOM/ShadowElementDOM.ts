8e60ed72b762fe0c44e27401df77ac54be01fb9f
drobler2019 <drobler2019@gmail.com>
2025-02-11 21:03:10 -0500

    Slot en Shadow DOM y light DOM

--- src/components/shadowDOM/ShadowElementDOM.ts
+++ src/components/shadowDOM/ShadowElementDOM.ts
@@ -3,21 +3,23 @@ import css from './shadowDOM.css?raw';
 export class ExampleShadowDOM extends HTMLElement {
 
     private readonly sheet = new CSSStyleSheet();
-    
+
     constructor() {
         super();
         this.attachShadow({ mode: "open" });
+        /* hace referencia al light DOM (es la parte visible del DOM que fué tapado por el shado DOM) */
+        this.innerHTML = '<p style="color: red">contenido por defecto</p>';
     }
 
     connectedCallback(): void {
-        this.shadowRoot!.innerHTML = /* html */`
-        <div class="shadow-dom">
-          <h1>HOLA MUNDO</h1>
-        </div>
-        <style>
-    </style>`;
         this.sheet.replace(css)
-            .then(() => this.shadowRoot!.adoptedStyleSheets.push(this.sheet));
+            .then(() => {
+                this.shadowRoot!.adoptedStyleSheets.push(this.sheet);
+                this.shadowRoot!.innerHTML = /* html */`
+                       <div class="shadow-dom">
+                          <h1>Hola usuario: <slot><p>desconocido</p></slot></h1>
+                       </div>`;
+            });
     }
 
     customElement(): void {
